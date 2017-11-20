3f50859986d424ee8719467d8532f7049ada495c
Margi Tran <margitran94@gmail.com>
2017-11-20 02:09:40 +0000

    test

--- message_handler.js
+++ message_handler.js
@@ -1,7 +1,5 @@
 var request = require('request')
 
-const token = "EAAFxZC8LaXgYBAJ9EJDT5U2XL00BnZADlH4OePZBvBO0FbR7da1ak9fgbyJ84GGje0TvTod1bH6ZCZAKYFLMJCyuB7lzzF6FFJ157zLFAkvqbQM9vZC58g2f5ZAYQZBtZAuzD9dhu7juSi0Q1cctCpZBjKvQ059P2LhzSfmgfCowifHC7SUVZBFsjCY"
-
 // from https://chatbotsmagazine.com/have-15-minutes-create-your-own-facebook-messenger-bot-481a7db54892
 exports.sendTextMessage = function(sender, text) {
     messageData = {
--- verification_handler.js
+++ verification_handler.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = (req, res) => {
-	if (req.query['hub.verify_token'] === 'my_token') 
+	if (req.query['hub.verify_token'] === process.env.FB_VERIFY_TOKEN) 
         res.status(200).send(req.query['hub.challenge']);
     else
 		res.status(403).send('Error: wrong token');
