c917d320f6f24c132dba5b0d7d1c5bfe1c953772
Margi Tran <margitran94@gmail.com>
2017-11-20 02:06:05 +0000

    test

--- message_handler.js
+++ message_handler.js
@@ -9,7 +9,7 @@ exports.sendTextMessage = function(sender, text) {
     }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: {access_token:process.env.FB_PAGE_ACCESS_TOKEN},
         method: 'POST',
         json: {
             recipient: {id:sender},
