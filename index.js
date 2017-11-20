2b79abbe959442f525afc139055ba03cf2d4cefb
Margi Tran <margitran94@gmail.com>
2017-11-20 01:02:04 +0000

    test

--- index.js
+++ index.js
@@ -2,55 +2,25 @@
 const verification_handler = require('./verification.js');
 const message_handler = require('./message_handler.js');
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-// Process application/x-www-form-urlencoded 
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
-app.use(bodyParser.json())
-
-// Spin up the server
+app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
+    console.log('Running on port', app.get('port'));
+});
 
-// Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
-})
+    res.send('Hello world, I am a chat bot');
+});
 
-/*
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_token') {
-        res.send(req.query['hub.challenge'])
-    }
-    res.send('Error, wrong token')
-})*/
 app.get('/', verification_handler);
-
 app.post('/webhook/', message_handler);
 
-/*
-// from https://chatbotsmagazine.com/have-15-minutes-create-your-own-facebook-messenger-bot-481a7db54892
-app.post('/webhook/', function (req, res) {
-	message_handler.handleMessage();
-    messaging_events = req.body.entry[0].messaging
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
-        if (event.message && event.message.text) {
-            text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-        }
-    }
-    res.sendStatus(200)
-})*/
 
 
