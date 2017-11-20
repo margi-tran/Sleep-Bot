948ce4162bf791bb65a03b964dd01138e3b508f6
Margi Tran <margitran94@gmail.com>
2017-11-20 00:04:31 +0000

    test

--- verification.js
+++ verification.js
@@ -9,9 +9,16 @@ module.exports = (req, res) => {
     } else {
         res.status(403).end();
     }*/
-	if (req.query['hub.verify_token'] === 'my_token') {
-        res.send(req.query['hub.challenge'])
+	
+	
+	/*if (req.query['hub.verify_token'] === 'my_token') {
+        //res.send(req.query['hub.challenge'])
     }
-    res.send('Error, wrong token')
+    res.send('Error, wrong token')*/
+	
+	if (req.query['hub.verify_token'] === 'my_token') 
+        res.status(200).send(req.query['hub.challenge'])
+    else
+		res.status(403).send('Error, wrong token')
 };
 
