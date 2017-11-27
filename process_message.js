47ff0e43ec8d10565973037e0da8c70d57ed3e7e
Margi Tran <margitran94@gmail.com>
2017-11-27 18:12:10 +0000

    cleaned up basic code v2

--- process_message.js
+++ process_message.js
@@ -10,14 +10,14 @@ module.exports = (event) => {
 	sender = event.sender.id;
 	message = event.message.text;
 	
-    sendMessage(sender, "[ok] Text received, echo: " + message.substring(0, 200));
+    sendMessage(sender, "[ok1] Text received, echo: " + message.substring(0, 200));
 	
 	if(message  === "!") {
 		sendMessage(sender, "Going to print your data!");
 	}
 };
 
-sendMessage = function(sender, text) {
+function sendMessage(sender, text) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: process.env.FB_PAGE_ACCESS_TOKEN},
@@ -33,4 +33,4 @@ sendMessage = function(sender, text) {
             console.log('Error: ', response.body.error);
         }
     });
-};
+}
