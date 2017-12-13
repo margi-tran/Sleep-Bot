a6a97cc32dadc8b66a1e12a124f56b5e35e7472b
Margi Tran <margitran94@gmail.com>
2017-12-13 22:02:23 +0000

    test

---
+++ webhook.js
@@ -0,0 +1,18 @@
+/**
+ * Module for handling Facebook messages recieved from the webhook.
+ */
+
+
+var processMessage = require('./process_message');
+
+module.exports = (req, res) => {
+    if (req.body.object === 'page') {
+        req.body.entry.forEach(entry => {
+            entry.messaging.forEach(event => {
+				if(event.message)
+					processMessage(event);
+         	});
+    	});
+    	res.status(200).end();
+    }
+};
