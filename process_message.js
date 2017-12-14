c54533693947741ff554552b45b08f1ad7976429
Margi Tran <margitran94@gmail.com>
2017-12-14 03:07:19 +0000

    test

--- process_message.js
+++ process_message.js
@@ -6,33 +6,6 @@
 
 var request = require('request');
 
-
-const send2 = (userId, messageData)  => {
-     console.log("IN HERE");
-    return new Promise((resolve, reject) => {
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {access_token: process.env.FB_PAGE_ACCESS_TOKEN},
-            method: "POST",
-            json: {
-                 recipient: {id: userId },
-                message: {text: text}
-            }
-        }, (error, response, body) => {
-                if (error) { 
-                    console.log("Error sending message: " + response.error); return reject(response.error); 
-                }
-                else if (response.body.error) { 
-                    console.log('Response body Error: ' + response.body.error); return reject(response.body.error); 
-                }
-
-                console.log("Message sent successfully to " + userId); 
-                return resolve(response);
-            }
-        );    
-    });
-};
-
 const send = (userId, messageData)  => {
     return new Promise((resolve, reject) => {
         request({
@@ -60,24 +33,13 @@ module.exports = (event) => {
 	sender = event.sender.id;
 	message = event.message.text;
 
-    /*send(sender, "Your userId: " + sender);
-    send(sender, "[OK] Text received! echoing: " + message.substring(0, 200)).then(function(results) {
-         sendMessage(sender, "You entered '!'");
-    }).catch(function (error) {
-        console.log('Error: ', error);
-    });*/
-
-    send(sender, "Your userId: " + sender).then(
+    /*send(sender, "Your userId: " + sender).then(
         send(sender, "[OK] Text received! echoing: " + message.substring(0, 200))).then(
         sendMessage(sender, "You entered '!'")
-        );
+    );*/
 
-
-    /*send(sender, "[OK] Text received! echoing: " + message.substring(0, 200)).then(function(results) {
-         sendMessage(sender, "You entered '!'");
-    }).catch(function (error) {
-        console.log('Error: ', error);
-    });*/
+    var a = ["1", "2", "3"] //my result is a array
+    sendTextMessages(sender, a, 0) //OK. It works for me :)
 };
 
 function sendMessage(sender, text) {
@@ -96,5 +58,28 @@ function sendMessage(sender, text) {
             console.log('Error: ', response.body.error);
         }
     });
+}
+
+
+
+function sendTextMessages(sender, text, i) {
+    if (i < text.length) {
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {access_token:token},
+            method: 'POST',
+            json: {
+                recipient: {id:sender},
+                message: {text:text[i]},
+            }
+        }, function(error, response, body) {
+            if (error) {
+                console.log('Error sending messages: ', error)
+            } else if (response.body.error) {
+                console.log('Error: ', response.body.error)
+            }
+            sendTextMessages(sender, text, i+1)
+        })
+    } else return
 }
 
