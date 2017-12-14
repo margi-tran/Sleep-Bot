6d659a3e5b57e51aafa9baa75391b13f2976427b
Margi Tran <margitran94@gmail.com>
2017-12-14 02:20:10 +0000

    test

--- process_message.js
+++ process_message.js
@@ -6,6 +6,7 @@
 
 var request = require('request');
 
+/*
 const send = (userId, messageData)  => {
      console.log("IN HERE");
     return new Promise((resolve, reject) => {
@@ -30,7 +31,31 @@ const send = (userId, messageData)  => {
             }
         );    
     });
-};
+};*/
+
+const send = (userId, messageData)  => {
+     console.log("IN HERE");
+    return new Promise((resolve, reject) => {
+       // request();   
+
+       request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.FB_PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: {text: text}
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+        
+    });
+}
 
 
 module.exports = (event) => {
