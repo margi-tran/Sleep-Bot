7b1f585b2c84dc45c8bd653f7be96296acc5138e
Margi Tran <margitran94@gmail.com>
2017-12-14 02:42:09 +0000

    test

--- process_message.js
+++ process_message.js
@@ -6,8 +6,8 @@
 
 var request = require('request');
 
-/*
-const send = (userId, messageData)  => {
+
+const send2 = (userId, messageData)  => {
      console.log("IN HERE");
     return new Promise((resolve, reject) => {
         request({
@@ -31,12 +31,10 @@ const send = (userId, messageData)  => {
             }
         );    
     });
-};*/
+};
 
 const send = (userId, messageData)  => {
     return new Promise((resolve, reject) => {
-       // request();   
-
        request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: process.env.FB_PAGE_ACCESS_TOKEN},
@@ -45,14 +43,16 @@ const send = (userId, messageData)  => {
             recipient: {id: userId},
             message: {text: messageData}
         }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
+        }, function(error, response, body) {
+            if (error) {
+                console.log('Error sending messages: ', error);
+            } else if (response.body.error) {
+                console.log('Error: ', response.body.error);
+            }
 
+            console.log("Message sent successfully to " + userId); 
+            return resolve(response);
+        });
     });
 }
 
@@ -66,9 +66,11 @@ module.exports = (event) => {
 	if(message  === "!") {
 		sendMessage(sender, "You entered '!'");
 	}*/
-send(sender, sender + " [OK] Text received, echo: " + message.substring(0, 200));
+
+    send(sender, "Your userId: " + sender);
+
     send(sender, "[OK] Text received, echo: " + message.substring(0, 200)).then(function(results) {
-        console.log("******************GOT HERE WOW");
+        sendMessage(sender, "You entered '!'");
     }). catch(function (error) {
         console.log("ERROR");
     });
