a3bb1cad6d825057aecba69739afc6a5a8bae2e9
Margi Tran <margitran94@gmail.com>
2017-12-27 02:16:26 +0000

    test

--- process_message.js
+++ process_message.js
@@ -20,6 +20,8 @@ module.exports = async (event) => {
     var query = {};
     const res1 = await testcollection.find(query).toArray();
     console.log(res1);
+    var val = res1[1]['user'];
+    console.log("**************USER:" + val);
     sendMessage(userId, "ok");
     return;
     
