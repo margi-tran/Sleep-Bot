0536fa454a8ed1ab0db997a8f13e21e3a6d3252a
Margi Tran <margitran94@gmail.com>
2017-12-27 13:25:49 +0000

    test

--- process_message.js
+++ process_message.js
@@ -24,7 +24,7 @@ module.exports = async (event) => {
     var v = JSON.stringify(val);
     console.log("STRING: " + val);
     console.log("STRING: " + v);
-    console.log("USERNAME:" + v.first);
+    console.log("USERNAME:" + val.first);
     sendMessage(userId, "ok");
     return;
     
