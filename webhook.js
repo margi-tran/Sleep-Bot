92e9584b397b9b569087f966fce19b2105c34062
Margi Tran <margitran94@gmail.com>
2017-12-28 17:14:50 +0000

    test

--- webhook.js
+++ webhook.js
@@ -5,7 +5,7 @@
 
 var processMessage = require('./process_message');
 
-
+/*
 module.exports = async (req, res) => {
 	try {
     if (req.body.object === 'page') {
@@ -14,9 +14,9 @@ module.exports = async (req, res) => {
 				if (event.message)
 					processMessage(event);
 
-				/*if(event.postback) {
+				if(event.postback) {
 					console.log("get started was pressed");
-				}*/
+				}
          	});
     	});
     	res.status(200).end();
@@ -24,7 +24,38 @@ module.exports = async (req, res) => {
     } catch (err) {
     	console.log('ERROR: ', err);
     }
-};
+};*/
+
+module.exports = async (req, res) => {
+
+  var data = req.body;
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+         if (messagingEvent.message) {
+          processMessage(event);
+        } else if (messagingEvent.postback) {
+          console.log("WOW");
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've 
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200);
+  }
+}
 
 
 
