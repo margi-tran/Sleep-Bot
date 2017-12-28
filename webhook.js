55b48773259d5e0456d20a534d4adc9f217500b6
Margi Tran <margitran94@gmail.com>
2017-12-28 22:54:03 +0000

    test

--- webhook.js
+++ webhook.js
@@ -30,53 +30,52 @@ module.exports = async (req, res) => {
 
 module.exports = async (req, res) => {
 	try {
-  var data = req.body;
-  // Make sure this is a page subscription
-  if (data.object == 'page') {
-    // Iterate over each entry
-    // There may be multiple if batched
+		var data = req.body;
+  
+  		// Make sure this is a page subscription
+  		if (data.object == 'page') {
+    	// Iterate over each entry
+    	// There may be multiple if batched
 
+    	if(data.entry === undefined) {
+    		console.log("err first");
+    		return;
+    	}
 
-    if(data.entry === undefined) {
-    	console.log("err first");
-    	return;
-    }
-	console.log('***************************data.entry:', data.entry);
+    	data.entry.forEach(function(pageEntry) {
+      		var pageID = pageEntry.id;
+      		var timeOfEvent = pageEntry.time;
 
-    data.entry.forEach(function(pageEntry) {
-      var pageID = pageEntry.id;
-      var timeOfEvent = pageEntry.time;
+      		if(pageEntry.messaging === undefined) {
+    			console.log("err second");
+    			return;
+    		}
+    
+      		// Iterate over each messaging event
+      		pageEntry.messaging.forEach(function(messagingEvent) {
+      			if (messagingEvent === null || messagingEvent === undefined) {
+      				console.log("GOTCHA");
+      				return;
+      			}
 
-      if(pageEntry.messaging === undefined) {
-    	console.log("err second");
-    	return;
-    }
+         		if (messagingEvent.message) {
+         			processMessage(messagingEvent);
+        		} else if (messagingEvent.postback) {
+        			console.log("WOW");
+        			processPostback(messagingEvent);
+        		} else {
+          			console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        		}
+      		});
+    	});
 
-      // Iterate over each messaging event
-      pageEntry.messaging.forEach(function(messagingEvent) {
-      	if (messagingEvent === null || messagingEvent === undefined) {
-      		console.log("GOTCHA");
-      		return;
-      	}
-
-         if (messagingEvent.message) {
-          processMessage(messagingEvent);
-        } else if (messagingEvent.postback) {
-        	console.log("WOW");
-        	processPostback(messagingEvent);
-        	//receivedPostback(messagingEvent);
-        } else {
-          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know you've 
-    // successfully received the callback. Otherwise, the request will time out.
-    res.sendStatus(200);
-  }
+    	// Assume all went well.
+    	//
+    	// You must send back a 200, within 20 seconds, to let us know you've 
+    	// successfully received the callback. Otherwise, the request will time out.
+    	res.sendStatus(200);
+  
+	} 
 } catch (err) {
 	console.log('find it:', err);
 }
