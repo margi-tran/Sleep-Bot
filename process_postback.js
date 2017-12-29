dc3fb601405edf7f2919e7a4a70f01166e07a12f
Margi Tran <margitran94@gmail.com>
2017-12-29 02:18:06 +0000

    test

--- process_postback.js
+++ process_postback.js
@@ -11,14 +11,33 @@ module.exports = async (event) => {
     try { 
         fbUserId = event.sender.id;
         //message = event.message.text;
-        
+
         sendMessage(fbUserId, 'postback');
 
         if(event.postback.payload === 'GET_STARTED_PAYLOAD') {
+            // check whether the user exists in the database
+            const db = await MongoClient.connect(process.env.MONGODB_URI);
+            query = { fbUserId_: fbUserId };
+            result = await db.collection('fitbitauths').find(query).toArray();
+            db.close();
 
-        sendMessage(fbUserId, 'ahhh');
-
-
+            if(result.length == 0) { // user is not in database
+                const db = await MongoClient.connect(process.env.MONGODB_URI);
+                var newUser = { fbUserId_: fbUserId, 
+                                fitbitId_: "raise",
+                                accessToken: "kappa",
+                                refreshAccessToken: "123" };
+                await db.collection('fitbitauths').insertOne(newUser);
+                db.close();
+                
+                m1 = 'I will need you to give me permission to access your data on Fitbit, so that I can analyze your sleep based on it.';
+                m2 = 'To do so click on the following link:';
+                m3 = 'https://calm-scrubland-31682.herokuapp.com/fitbit'
+                sendMultipleMessages(fbUserId, [m1, m2, m3], 0); 
+            } else { // user is in database
+                sendMessage(fbUserId, 'Welcome back!');
+            }
+            return;
         }
 
     } catch (err) {
