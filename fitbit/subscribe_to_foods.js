0fea4ebb6214ed0c0682ddcb6778101355257f00
Margi Tran <margitran94@gmail.com>
2018-01-03 19:15:06 +0000

    test

--- fitbit/subscribe_to_foods.js
+++ fitbit/subscribe_to_foods.js
@@ -7,8 +7,7 @@ module.exports = (client, accessToken) => {
     requestUrl = "/foods/apiSubscriptions/1.json";
     console.log(requestUrl);
     client.post(requestUrl, accessToken).then(function(results) {
-        console.log('subscribeToFoods():', results);
-        console.log(results[0]);
+        console.log('subscribeToFoods():', results[0]);W
     }).catch(function(results) {
         console.log(results[0].errors);
     })
