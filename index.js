2f9f528637adfd8eb277e1ef6531192d97502787
Margi Tran <margitran94@gmail.com>
2018-01-03 04:17:45 +0000

    test

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get('/fitbit_oauth_callback', async (req, res) => {
 		//m1 = 'Great! You have given me permission to access your health data on Fitbit.';
 		//m2 = 'First, I would like to get an idea about your current sleep health so I\' going to ask you a few questions.';
 	} catch (err) {
-		res.send('[ERROR]', err);
+		res.send('[ERROR]' + err);
 	}
 });
 
