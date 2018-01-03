91cf3c460af700b0f48cb3824b913d04f0b1f926
Margi Tran <margitran94@gmail.com>
2018-01-03 23:21:31 +0000

    test

--- index.js
+++ index.js
@@ -106,6 +106,17 @@ app.get('/fitbit_webhook', (req, res) => {
 });
 
 app.post('/fitbit_webhook', (req, res) => {
+	try {
 	console.log(req.body);
+	fitbitId = req.body[0].ownerId;
+	date = req.body[0].date;
+	console.log('daaaaaa', fitbitId, date)
+	
+
+
+
     res.sendStatus(204);
+} catch (err) {
+	console.log('[ERROR]', err)
+}
 });
