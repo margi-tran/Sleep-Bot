d00e0e9f9b7e0e53ef7422f66d8fe70d9a783498
Margi Tran <margitran94@gmail.com>
2018-01-03 02:19:33 +0000

    test

--- facebook/test.js
+++ facebook/test.js
@@ -1,7 +1,7 @@
 var fbMessengerBot = require('fb-messenger-bot-api');
 var fbMessengerBotClient = new fbMessengerBot.Client(process.env.FB_PAGE_ACCESS_TOKEN);
 
-exports.sendTextMessage = async (fbUserId, message) {
+exports.sendTextMessage = async (fbUserId, message) => {
 	return new Promise(function(resolve, reject) => {
 		try {
 			result = await fbMessengerBotClient.sendTextMessage(fbUserId, message);
