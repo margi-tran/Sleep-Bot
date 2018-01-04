701437c53b5af793b39bd717fe233b4a82b0eb1d
Margi Tran <margitran94@gmail.com>
2018-01-04 19:44:34 +0000

    test

--- route_handlers/fitbit/redirect.js
+++ route_handlers/fitbit/redirect.js
@@ -1,5 +1,5 @@
 var fitbitClient = require('../../utility/fitbit_client');
 
 module.exports = (req, res) => {
-	res.redirect(fitbitClient.client.getAuthorizeUrl(fitbitClient.client.scope, fitbitClient.client.redirectUri));
+	res.redirect(fitbitClient.client.getAuthorizeUrl(fitbitClient.scope, fitbitClient.redirectUri));
 };
