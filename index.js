a39f613ae7bf0f346688a83c1d39ad6ba1d9df50
Margi Tran <margitran94@gmail.com>
2018-01-04 03:41:18 +0000

    test

--- index.js
+++ index.js
@@ -135,7 +135,8 @@ app.get('/view', async (req, res) => {
 		refreshAccessTokenPromise = await client.refreshAccessToken(oldAccessToken, oldRefreshAccessToken);
 		var newAccessToken = refreshAccessTokenPromise.access_token;
 		var newRefreshToken = refreshAccessTokenPromise.refresh_token;
-		await db.collection('fitbit_auths').updateOne(fitbitId, { accessToken: "INTING", refreshAccessToken: "kek" });
+		await db.collection('fitbit_auths').updateOne({fitbitId_: fitbitId}, 
+								{ accessToken: "INTING", refreshAccessToken: "kek" });
 
 		res.send(refreshAccessTokenPromise);
 	} catch (err) {
