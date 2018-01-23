8a673530ea75e36d6a6fb61bffbebab70038de7f
Margi Tran <margitran94@gmail.com>
2018-01-23 19:48:59 +0000

    commit

--- route_handlers/fitbit/webhook_post.js
+++ route_handlers/fitbit/webhook_post.js
@@ -10,16 +10,14 @@ var dateAndTimeUlti = require('../../utility/date_and_time_util');
 
 module.exports = async (req, res) => {
 	try {
-
-		console.log(req.body);
-
 		var notifications = req.body;
+		notifications.forEach(notification => {
+			console.log(notification);
+		});
 
 
+		
 		const fitbitId = req.body[0].ownerId;
-
-
-		console.log(req.body);
 		var date = req.body[0].date;
 		console.log('daaaaaa', fitbitId, date);
 
