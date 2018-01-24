4f1ade0387f2f2ea14148fa7e066e8e9e7dc5d07
Margi Tran <margitran94@gmail.com>
2018-01-24 16:49:06 +0000

    commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var fbMessengerBotClient = new fbMessengerBot.Client(process.env.FB_PAGE_ACCESS_
 var MessengerBot = require('messenger-bot');
 var messengerBotClient = new MessengerBot({ token:process.env.FB_PAGE_ACCESS_TOKEN });
 
-var rootHandler = require('./controllers/route_handlers');
+var routeHandlers = require('./controllers/route_handlers');
 var fitbitClient = require('./controllers/fitbit/fitbit_client');
 var dateAndTimeUlti = require('./utility/date_and_time_util');
 var jobs = require('./jobs');
