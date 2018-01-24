d9084113379239b4e5c5e6019f9d7a40633c73dd
Margi Tran <margitran94@gmail.com>
2018-01-24 04:34:43 +0000

    commit

--- route_handlers/fitbit/webhook_post.js
+++ route_handlers/fitbit/webhook_post.js
@@ -10,49 +10,38 @@ var dateAndTimeUlti = require('../../utility/date_and_time_util');
 
 module.exports = async (req, res) => {
 	try {
-
-		console.log(req.body); 
-		
 		var notifications = req.body;
 		notifications.forEach(async (notification) => {
-			console.log(notification);
+			const fitbitId = notification.ownerId;
+			const date = notification.date;
 
-		
+			const db = await MongoClient.connect(process.env.MONGODB_URI);
+    		const result = await db.collection('fitbit_auths').find({ fitbitId_: fitbitId }).toArray();
 
-		
-		//const fitbitId = req.body[0].ownerId;
-		const fitbitId = notification.ownerId;
-		const date = req.body[0].date;
+    		// Refresh the user's access token
+    		var oldAccessToken = result[0].accessToken;
+    		var oldRefreshAccessToken = result[0].refreshAccessToken;
+			var refreshAccessTokenPromise = await fitbitClient.client.refreshAccessToken(oldAccessToken, oldRefreshAccessToken);
+			var newAccessToken = refreshAccessTokenPromise.access_token;
+			var newRefreshToken = refreshAccessTokenPromise.refresh_token;
+			await db.collection('fitbit_auths').updateOne({ fitbitId_: fitbitId }, { $set: { accessToken: newAccessToken, refreshAccessToken: newRefreshToken } });
 
-		const db = await MongoClient.connect(process.env.MONGODB_URI);
-    	const result = await db.collection('fitbit_auths').find({ fitbitId_: fitbitId }).toArray();
+			// Get the user's sleep data
+			fbUserId = result[0].fbUserId_;
+   			var accessToken = result[0].accessToken;
+    		const sleepData = await fitbitClient.client.get('/sleep/date/' + dateAndTimeUlti.dateToString(new Date()) + '.json', newAccessToken);
+  			
+  			var sleepDataDoc = 
+  				{ 
+  					fbUserId_: fbUserId,
+  					date: date,
+  					sleep_data: sleepData[1].body.sleep;
+            	};
+        	db.collection('sleep_data').update({ fbUserId_: fbUserId, date: date }, sleepDataDoc, { upsert : true });
 
-    	// Refresh the user's access token
-    	var oldAccessToken = result[0].accessToken;
-    	var oldRefreshAccessToken = result[0].refreshAccessToken;
-		var refreshAccessTokenPromise = await fitbitClient.client.refreshAccessToken(oldAccessToken, oldRefreshAccessToken);
-		var newAccessToken = refreshAccessTokenPromise.access_token;
-		var newRefreshToken = refreshAccessTokenPromise.refresh_token;
-		await db.collection('fitbit_auths').updateOne({ fitbitId_: fitbitId }, { $set: { accessToken: newAccessToken, refreshAccessToken: newRefreshToken } });
-
-		// Get the user's sleep data
-		fbUserId = result[0].fbUserId_;
-   		var accessToken = result[0].accessToken;
-    	const sleepData = await fitbitClient.client.get('/sleep/date/' + dateAndTimeUlti.dateToString(new Date()) + '.json', newAccessToken);
-
-    	var arr = sleepData[1].body.sleep;
-    	console.log(arr);
-  		var sleepDataDoc = 
-  			{ 
-  				fbUserId_: fbUserId,
-  				date: date,
-  				sleep_data: arr
-            };
-        db.collection('sleep_data').update({ fbUserId_: fbUserId, date: date }, sleepDataDoc, { upsert : true });
-
-    	res.sendStatus(204);
-    	db.close();
-    });
+    		res.sendStatus(204);
+    		db.close();
+    	});
 	} catch (err) {
 		console.log('[ERROR]', err);
 	}
