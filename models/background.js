716c5892568bc6070f3096b57a43ee3adf33e483
Margi Tran <margitran94@gmail.com>
2018-01-24 17:23:27 +0000

    commit

--- models/background.js
+++ models/background.js
@@ -1,6 +1,8 @@
-const db = await MongoClient.connect(process.env.MONGODB_URI);
+var MongoClient = require('mongodb').MongoClient;
 
 exports.getBackground = async (fbUserId) => {
+	const db = await MongoClient.connect(process.env.MONGODB_URI);
     const result = await db.collection('background').find({ fbUserId_: fbUserId }).toArray();
+    db.close();
     return result;
 };
--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
-const db = await MongoClient.connect(process.env.MONGODB_URI);
+var MongoClient = require('mongodb').MongoClient;
 
 exports.updateBotRequested = async (fbUserId, nextQuestion) => {
+	const db = await MongoClient.connect(process.env.MONGODB_URI);
     await db.collection('users').updateOne({ fbUserId_: fbUserId }, { $set: { botRequested: nextQuestion } });
+    db.close();
 };
