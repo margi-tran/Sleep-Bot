cedffdd39a317aec13b839acea8bad065aa4cfc8
Margi Tran <margitran94@gmail.com>
2018-01-25 04:17:49 +0000

    CHECKPOINT

--- controllers/fitbit/webhook_post.js
+++ controllers/fitbit/webhook_post.js
@@ -17,17 +17,6 @@ module.exports = async (req, res) => {
 			const fitbitId = notification.ownerId;
 			const date = notification.date;
 
-		  	//const db = await MongoClient.connect(process.env.MONGODB_URI);
-		   	//onst result = await db.collection('fitbit_auths').find({ fitbitId_: fitbitId }).toArray();
-
-			// Refresh the user's access token
-			/*var oldAccessToken = result[0].accessToken;
-			var oldRefreshAccessToken = result[0].refreshAccessToken;
-			var refreshAccessTokenPromise = await fitbitClient.client.refreshAccessToken(oldAccessToken, oldRefreshAccessToken);
-			var newAccessToken = refreshAccessTokenPromise.access_token;
-			var newRefreshToken = refreshAccessTokenPromise.refresh_token;
-			await db.collection('fitbit_auths').updateOne({ fitbitId_: fitbitId }, { $set: { accessToken: newAccessToken, refreshAccessToken: newRefreshToken } });
-*/
 			var oldAccessToken = await fitbitAuth.getAccessToken(fitbitId);
 			var oldRefreshAccessToken = await fitbitAuth.getRefreshAccessToken(fitbitId);
 			var refreshAccessTokenPromise = await fitbitClient.client.refreshAccessToken(oldAccessToken, oldRefreshAccessToken);
@@ -35,17 +24,9 @@ module.exports = async (req, res) => {
 			var newRefreshToken = refreshAccessTokenPromise.refresh_token;
 			await fitbitAuth.updateFitbitTokens(fitbitId, newAccessToken, newRefreshToken);
 
-			// Get the user's sleep data
+			// Get the user's sleep data from Fitbit and put it into the database
 			var fbUserId = await fitbitAuth.getFbUserId(fitbitId);
 			const sleepData = await fitbitClient.client.get('/sleep/date/' + dateAndTimeUlti.dateToString(new Date()) + '.json', newAccessToken);
-			/*var sleepDataDoc = 
-				{ 
-					fbUserId_: fbUserId,
-					date: date,
-					sleep_data: sleepData[1].body
-				};
-			db.collection('sleep_data').update({ fbUserId_: fbUserId, date: date }, sleepDataDoc, { upsert : true });
-			*/
 			sleep.insertSleepData(fbUserId, date, sleepData[1].body);
 
 			res.sendStatus(204);
