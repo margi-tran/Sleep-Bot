f34f72e47fd1c4d551e0a24b5d3ea12b3476983a
Margi Tran <margitran94@gmail.com>
2018-01-27 20:26:17 +0000

    commit

--- models/sleep.js
+++ models/sleep.js
@@ -12,12 +12,12 @@ exports.insertSleepData = async (fbUserId, date, sleepData) => {
     db.close();
 };
 
-exports.getMainSleep = async (fbUserId, date) => {
+/*exports.getMainSleep = async (fbUserId, date) => {
     const db = await MongoClient.connect(process.env.MONGODB_URI);
     const result = await db.collection('sleep_data').find({ fbUserId_: fbUserId, date: date }).toArray();
     var sleepArr = result[0].sleep_data.sleep;
 
-    /*if (sleepArr === null || sleepArr === []) 
+    if (sleepArr === null || sleepArr === []) 
     	return null;
     else 
     	sleepArr.forEach(function(sleepItem) {
@@ -26,11 +26,22 @@ exports.getMainSleep = async (fbUserId, date) => {
         		console.log('in here');
         		return 'ok';
         	}
-    	});*/
+    	});
 
-    sleepArr.forEach(function(sleepItem) {
-        if(sleepItem.isMainSleep) return 'yap';
+
+
+    return 'ok';
+};*/
+
+exports.getMainSleep = async (fbUserId, date) => {
+	var arr = [];
+    const db = await MongoClient.connect(process.env.MONGODB_URI);
+    const users = await db.collection('users').find().toArray();
+    db.close();
+    users.forEach(function(user) {
+        if(user.notifiedSleep === false) arr.push(user.fbUserId_);
     });
-
-    	return 'ok';
+    return arr;
 };
+
+
