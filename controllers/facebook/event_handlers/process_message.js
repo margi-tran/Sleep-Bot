633c1fa71a33a0533b974b07fb2502bbc0be1042
Margi Tran <margitran94@gmail.com>
2018-02-01 10:28:11 +0000

    commit

--- controllers/facebook/event_handlers/process_message.js
+++ controllers/facebook/event_handlers/process_message.js
@@ -41,11 +41,15 @@ module.exports = async (event) => {
 
         console.log('you wrote: ', message);
 
-        if(message === 'why is using electronic devices bad for sleep?')
+        if(message === 'why is using electronic devices bad for sleep?') {
             fbMessengerBotClient.sendTextMessage(fbUserId, 'They emit blue light which can trick your brain to think its daytime');
+            return;
+        }
 
-        if(message === 'what does alcohol do to sleep?')
+        if(message === 'what does alcohol do to sleep?') {
             fbMessengerBotClient.sendTextMessage(fbUserId, 'Although it makes you sleep, it will wear off during the night and make your brain active.');
+            return;
+        }
 
         fbMessengerBotClient.sendTextMessage(fbUserId, 'Sorry I dont understand you.');
     } catch (err) {
