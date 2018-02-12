21697d5ca0c7a7bcfd4f9ae25d73994df503452a
Margi Tran <margitran94@gmail.com>
2018-02-12 06:36:04 +0000

    commit

--- controllers/facebook/event_handlers/process_message.js
+++ controllers/facebook/event_handlers/process_message.js
@@ -238,7 +238,7 @@ async function getNewUserBackground(fbUserId, message, event, mainContext) {
                             updateContextsAndAskNextQuestion(fbUserId, constants.BACKGROUND_STRESSED, constants.QUESTION_ANSWER, constants.BACKGROUND_STRESSED, true);
                         }
                     } else {
-                        repeatQuestion(fbUserId, backgroundQuestionsMap[constants.BACKGROUND_ELECTRONICS], true);
+                        repeatQuestion(fbUserId, backgroundQuestionsMap[constants.ELECTRONICS], true);
                     }
                 } else if (subContext === constants.QUESTION_ANSWER_DONE) {
                     if (message === 'why') {
