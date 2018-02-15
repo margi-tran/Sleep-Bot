e40f8ed09fe8d3b777871ff7f913fbcb37726b6b
Margi Tran <margitran94@gmail.com>
2018-02-15 02:54:12 +0000

    commit

--- models/sleep.js
+++ models/sleep.js
@@ -31,7 +31,7 @@ async function getMainSleep(fbUserId, date) {
     db.close();
     if (result.length === 0) return null;
     var sleepArr = result[0].sleep_data.sleep;
-    if (sleepArr.length === 0) return null;
+    if (sleepArr === null || sleepArr.length === 0) return null;
     for (i = 0; i < sleepArr.length; i++) {
         sleepItem = sleepArr[i];
         if (sleepItem.isMainSleep) 
