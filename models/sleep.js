e6b73e6e5afa379800254ee5dc327ac306e75e65
Margi Tran <margitran94@gmail.com>
2018-02-15 02:37:09 +0000

    commit

--- models/sleep.js
+++ models/sleep.js
@@ -21,7 +21,7 @@ exports.mainSleepExists = async (fbUserId, date) => {
 exports.getMainSleepLevelsData = async (fbUserId, date) => {
     var mainSleep = await getMainSleep(fbUserId, date);
     if (mainSleep === null) return null;
-    console.log('lala', mainSleep.levels.data);
+    console.log('lala', mainSleep.levels);
     else mainSleep.levels.data;
 };
 
