fb8c5737b1e5c61d8ee5a95ba88612d3d90bc15d
Margi Tran <margitran94@gmail.com>
2018-02-15 01:49:51 +0000

    commit

--- controllers/facebook/event_handlers/process_message.js
+++ controllers/facebook/event_handlers/process_message.js
@@ -889,6 +889,9 @@ async function givePersonalSleepAdvice(fbUserId) {
 
     var sleepDataArr = [];
     for (var i = 0; i < 7; i++) {
+        var mainSleepExists = await sleep.mainSleepExists(fbUserId, dateArr[i]);
+        if (mainSleepExists === false) continue;
+        
         var mainSleepLevelsData = await sleep.getMainSleepLevelsData(fbUserId, dateArr[i]);
         if (mainSleepLevelsData) sleepDataArr.push(mainSleepLevelsData);
     }
