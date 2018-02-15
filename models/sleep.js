7d9685461e0c1b9f830f20e5361b7edc868802b0
Margi Tran <margitran94@gmail.com>
2018-02-15 02:23:04 +0000

    commit

--- models/sleep.js
+++ models/sleep.js
@@ -14,7 +14,6 @@ exports.insertSleepData = async (fbUserId, date, sleepData) => {
 
 exports.mainSleepExists = async (fbUserId, date) => {
     var flag = await getMainSleep(fbUserId, date);
-    console.log(date, flag);
     if (flag === null) return false;
     else return true;
 };
@@ -29,7 +28,7 @@ async function getMainSleep(fbUserId, date) {
     const db = await MongoClient.connect(process.env.MONGODB_URI);
     const result = await db.collection('sleep_data').find({ fbUserId_: fbUserId, date: date }).toArray();
     db.close();
-    //console.log(date, result);
+    console.log(date, result.length);
     if (result.length === 0) return null;
     var sleepArr = result[0].sleep_data.sleep;
     if (sleepArr === null || sleepArr.length === 0) return null;
