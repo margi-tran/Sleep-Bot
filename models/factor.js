a017a35565dd5fa1d696111f52b6713ce4ca8728
Margi Tran <margitran94@gmail.com>
2018-02-16 23:12:26 +0000

    commit

--- models/factor.js
+++ models/factor.js
@@ -11,6 +11,5 @@ exports.getConsequences = async () => {
 	const db = await MongoClient.connect(process.env.MONGODB_URI);
     const result = await db.collection('factors').find({ type: 'consequences' }).toArray();
     db.close();
-    console.log('kaaaaaaaapa',result.explanations);
-    return result.explanations;
+    return result[0].explanations;
 }
