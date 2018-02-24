e660f79679741c59cb5d2286b853c92c1d186948
Margi Tran <margitran94@gmail.com>
2018-02-24 18:03:00 +0000

    FINAL

--- controllers/sleep/notifier.js
+++ controllers/sleep/notifier.js
@@ -10,7 +10,7 @@ var sleep = require('../../models/sleep');
 var constants = require('../constants');
 var dateAndTimeUtil = require('../../utility/date_and_time_util');
 
-schedule.scheduleJob('02 9-22 * * *', notifySleep);
+schedule.scheduleJob('04 9-22 * * *', notifySleep);
 //schedule.scheduleJob('0 9-22 * * *', notifySleep);
 schedule.scheduleJob('0 0 * * *', resetNotifyFlag);
 
