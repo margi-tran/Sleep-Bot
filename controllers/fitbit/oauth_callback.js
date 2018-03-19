13ae95fa46fcfc088c6fd26ee076f2998fc8d51f
Margi Tran <margitran94@gmail.com>
2018-03-19 01:47:12 +0100

    FINAL

--- controllers/fitbit/oauth_callback.js
+++ controllers/fitbit/oauth_callback.js
@@ -42,7 +42,9 @@ module.exports = async (req, res) => {
         await user.setMainContext(fbUserId, null);
         await userBackground.addNewUserBackground(fbUserId, profileData[0].user.age);
 
-    	fitbitClient.client.post('/sleep/apiSubscriptions/1.json', accessTokenPromise.access_token).then((results) => {
+    	//fitbitClient.client.post('/sleep/apiSubscriptions/1.json', accessTokenPromise.access_token).then((results) => {
+    	var postStr = '/sleep/apiSubscriptions/' + fbUserId + '.json';
+    	fitbitClient.client.post(postStr, accessTokenPromise.access_token).then((results) => {
        		console.log(results);
             console.log('subscribeToSleep:', results[0]);
     	}).catch((results) => {
