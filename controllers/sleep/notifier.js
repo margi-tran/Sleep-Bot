8db45ecba97e7c7fb4b8a8d53d6deeb8b12c75f5
Margi Tran <margitran94@gmail.com>
2018-03-19 03:56:19 +0100

    FINAL

--- controllers/sleep/notifier.js
+++ controllers/sleep/notifier.js
@@ -9,8 +9,8 @@ var sleep = require('../../models/sleep');
 
 var constants = require('../constants');
 var dateAndTimeUtil = require('../../utility/date_and_time_util');
-
-schedule.scheduleJob('0 9-22 * * *', notifySleep);
+schedule.scheduleJob('58 * * * *', notifySleep);
+//schedule.scheduleJob('0 9-22 * * *', notifySleep);
 schedule.scheduleJob('0 0 * * *', resetNotifyFlag);
 
 async function notifySleep() {
@@ -33,16 +33,46 @@ async function notifySleep() {
 		var maxAwake = 0; // Time awake in seconds
 		var tmp = 0;
 		var timeOfAwake = 0;
+
+		var startTimeOfSleep = dateAndTimeUtil.getTimeFromDateString(mainSleepLevelsData[0].dateTime);
+    	var hour1 = dateAndTimeUtil.getHourFromTimeString(startTimeOfSleep);
+    	var min1 = dateAndTimeUtil.getMinuteFromTimeString(startTimeOfSleep);
+    	var date1 = new Date(2018, 1, 1, hour1, min1);
+
 		for (var j = 0; j < lengthOfData; j++) {
-			timeOfAwake = dateAndTimeUtil.getTimeFromDateString(mainSleepLevelsData[j].dateTime);
-			for (var k = j; k < lengthOfData; k++) {
-				var data = mainSleepLevelsData[k];
-				if (data.level === 'awake' || data.level === 'restless' || data.level === 'wake') tmp += data.seconds;
-				else break;
-			}
-			if (tmp > maxAwake) maxAwake = tmp;
-			tmp = 0;
-		}
+        	timeOfAwake = dateAndTimeUtil.getTimeFromDateString(mainSleepLevelsData[j].dateTime);
+        	for (var k = j; k < lengthOfData; k++) {
+            	var data = mainSleepLevelsData[k];
+            	if (data.level === 'awake' || data.level === 'restless') {
+
+                    var time2 = dateAndTimeUtil.getTimeFromDateString(data.dateTime);
+                    var hour2 = dateAndTimeUtil.getHourFromTimeString(time2);
+                    var min2 = dateAndTimeUtil.getMinuteFromTimeString(time2);
+                    var date2 = new Date(2018, 1, 1, hour2, min2);
+                    var difference = (new Date(date2 - date1)).getMinutes();
+
+                    console.log(startTimeOfSleep, time2);
+                    console.log('test:', difference);
+
+                    if (difference > 15) tmp += data.seconds;
+            } else {
+                break;
+            }
+        }
+        	if (tmp > maxAwake) maxAwake = tmp;
+        	tmp = 0;
+        	if (maxAwake >= 600) break;
+    	}
+
+
+
+
+
+
+
+
+
+
 
 		var minutesAsleep = await sleep.getMinutesAsleep(fbUserId, date);
 
