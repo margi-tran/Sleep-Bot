b8e9f39da14fadc81bb054b9cdfa4e7ac462dc8e
Margi Tran <margitran94@gmail.com>
2018-03-26 20:45:24 +0100

    FINAL

--- controllers/fitbit/fitbit_client.js
+++ controllers/fitbit/fitbit_client.js
@@ -2,7 +2,7 @@ var Fitbit = require('fitbit-node');
 var client = new Fitbit(process.env.FITBIT_CLIENT_ID , process.env.FITBIT_CLIENT_SECRET);
 
 var redirectUri = 'https://calm-scrubland-31682.herokuapp.com/fitbit_oauth_callback';
-var scope = 'activity heartrate location nutrition profile settings sleep social weight';
+var scope = 'profile sleep';
 
 module.exports = {
 	client,
